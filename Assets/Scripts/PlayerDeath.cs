2fcc7efdb8e6c1ace978179114ac209f973e98d8
ib-rim <ib.bashir@hotmail.com>
2021-10-25 19:34:02 +0100

    Changed respawn mechanic to player location change

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -7,7 +7,6 @@ public class PlayerDeath : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "FallThreshold")
         {
-            Destroy(gameObject);
             LevelManager.instance.Respawn();
         }
     }
