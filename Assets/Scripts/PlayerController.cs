3be6a9f5e27828779f21373ed612546eac47266f
Atikah <atikah.hussain@hotmail.co.uk>
2021-10-25 13:37:47 +0100

    Delete code for Obstacle collision (to replace)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -52,8 +52,6 @@ public class PlayerController : MonoBehaviour
     //     }
     // }
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Obstacle") {
             livesRemaining -= 1;
             livesText.text = "Lives: " + livesRemaining.ToString();
         }
