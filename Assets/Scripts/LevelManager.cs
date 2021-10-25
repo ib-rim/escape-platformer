2b6e950c8a98e821c80e35ebbecbbe8eb3bb8d20
Atikah <atikah.hussain@hotmail.co.uk>
2021-10-25 20:26:52 +0100

    Merge playerdeath2 to obstacle-collision

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+   public static LevelManager instance;
+
+   public Transform respawnPoint;
+   public GameObject playerPrefab;
+
+   private void Awake() {
+       instance = this;
+   }
+
+   public void Respawn() {
+       playerPrefab.GetComponent<Rigidbody2D>().position = respawnPoint.position;
+   }
+}
---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "FallThreshold")
+        {
+            LevelManager.instance.Respawn();
+        }
+    }
+
+}
