bd6cc7e4e4686d6561fa80caf1390423696c36a4
Atikah <atikah.hussain@hotmail.co.uk>
2021-10-25 22:13:23 +0100

    Add lag time between pitfall collision and pitfall disappearing

--- Assets/Scripts/PlayerScripts/PlayerCollisions.cs
+++ Assets/Scripts/PlayerScripts/PlayerCollisions.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class PlayerCollisions : MonoBehaviour
 {
     // Code for all of the Player's non-fatal collisions 
-    private void OnCollisionExit2D(Collision2D collision)
+
+    public int pitfallLag = 3; 
+
+    IEnumerator PitfallLag() {
+        yield return new WaitForSecondsRealtime(pitfallLag);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Pitfall")
         {
+            StartCoroutine("PitfallLag");
             collision.gameObject.SetActive(false);
         }
     }
