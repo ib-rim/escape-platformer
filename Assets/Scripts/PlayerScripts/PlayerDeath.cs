80a1e499a40392168889f89e338eb9b857acf233
ib-rim <ib.bashir@hotmail.com>
2021-10-28 18:24:10 +0100

    Added invincibility check to PlayerDeath

--- Assets/Scripts/PlayerScripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerScripts/PlayerDeath.cs
@@ -7,24 +7,29 @@ public class PlayerDeath : MonoBehaviour {
     
     private int livesRemaining = 3;
     public Text livesText;
+    public bool invincible;
 
     private void Start() {
         livesText.text = "Lives: " + livesRemaining.ToString();
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.tag == "FallThreshold")
-        {
-            LevelManager.instance.Respawn();
-            livesRemaining -= 1;
-            livesText.text = "Lives: " + livesRemaining.ToString();
-        }
-        
-        if (other.gameObject.tag == "Spikes")
-        {   
-            LevelManager.instance.Respawn();
-            livesRemaining -= 1;
-            livesText.text = "Lives: " + livesRemaining.ToString();
+    private void OnCollisionStay2D(Collision2D other) {
+
+        if(!invincible) {
+
+            if (other.gameObject.tag == "FallThreshold")
+            {
+                LevelManager.instance.Respawn();
+                livesRemaining -= 1;
+                livesText.text = "Lives: " + livesRemaining.ToString();
+            }
+            
+            if (other.gameObject.tag == "Spikes")
+            {   
+                LevelManager.instance.Respawn();
+                livesRemaining -= 1;
+                livesText.text = "Lives: " + livesRemaining.ToString();
+            }
         }
     }
 }
