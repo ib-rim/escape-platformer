22e79978b7d34864eb3b15f1e0131395601ae182
ib-rim <ib.bashir@hotmail.com>
2021-10-29 21:30:23 +0100

    Added SlowFall powerup

--- Assets/Scripts/Powerups.cs
+++ Assets/Scripts/Powerups.cs
@@ -9,8 +9,9 @@ public class Powerups : MonoBehaviour
     Color playerColor = new Color32(255, 255, 255, 255);
     Color jumpColor = new Color32(125, 212, 144, 255); 
     Color speedColor = new Color32(236, 108, 0, 255); 
-    Color slowColor = new Color32(215, 190, 137, 255);
+    Color slowFallColor = new Color32(201, 181, 179, 255); 
     Color invincibilityColor = new Color32(236, 225, 0, 255);
+    Color slowColor = new Color32(215, 190, 137, 255);
 
     public float powerupTime = 3;
 
@@ -19,60 +20,66 @@ public class Powerups : MonoBehaviour
         player = GetComponent<PlayerController>();
     }
 
+    public void resetPlayer() {
+        player.jumpSpeed = PlayerController.defaultJumpSpeed;
+        player.moveSpeed = PlayerController.defaultMoveSpeed;
+        GetComponent<PlayerDeath>().invincible = false;
+        GetComponent<Rigidbody2D>().gravityScale = 1f;
+        rend.material.color = playerColor;
+    }
+
     void OnTriggerEnter2D(Collider2D other) {
 
         if(other.gameObject.tag == "JumpBoost") {
             other.gameObject.SetActive(false);
-            StartCoroutine(boostJump());
+            player.jumpSpeed = 8f;
+            rend.material.color = jumpColor;
+            StartCoroutine(waitAndReset());
         }   
 
         if(other.gameObject.tag == "SpeedBoost") {
             other.gameObject.SetActive(false);
-            StartCoroutine(boostSpeed());
+            player.moveSpeed = 8f;
+            rend.material.color = speedColor;
+            StartCoroutine(waitAndReset());
         } 
 
-        if(other.gameObject.tag == "Slow") {
+        if(other.gameObject.tag == "SlowFall") {
             other.gameObject.SetActive(false);
-            StartCoroutine(slow());
+            rend.material.color = slowFallColor;
+            StartCoroutine(slowFall());
         } 
 
         if(other.gameObject.tag == "Invincibility") {
             other.gameObject.SetActive(false);
-            StartCoroutine(setInvincible());
+            rend.material.color = invincibilityColor;
+            GetComponent<PlayerDeath>().invincible = true;
+            StartCoroutine(waitAndReset());
+        }
+
+        if(other.gameObject.tag == "Slow") {
+            other.gameObject.SetActive(false);
+            player.moveSpeed = 2f;
+            player.jumpSpeed = 2f;
+            rend.material.color = slowColor;
+            StartCoroutine(waitAndReset());
         } 
     }
 
-    IEnumerator boostJump() {
-        player.jumpSpeed = 8f;
-        rend.material.color = jumpColor;
+    IEnumerator waitAndReset() {
         yield return new WaitForSeconds(powerupTime);
-        player.jumpSpeed = PlayerController.defaultJumpSpeed;
-        rend.material.color = playerColor;
+        resetPlayer();
     }
 
-    IEnumerator boostSpeed() {
-        player.moveSpeed = 8f;
-        rend.material.color = speedColor;
-        yield return new WaitForSeconds(powerupTime);
-        player.moveSpeed = PlayerController.defaultMoveSpeed;
-        rend.material.color = playerColor;
-    }
-
-    IEnumerator slow() {
-        player.moveSpeed = 2f;
-        player.jumpSpeed = 2f;
-        rend.material.color = slowColor;
-        yield return new WaitForSeconds(powerupTime);
-        player.moveSpeed = PlayerController.defaultMoveSpeed;
-        player.jumpSpeed = PlayerController.defaultJumpSpeed;
-        rend.material.color = playerColor;
-    }
-
-    IEnumerator setInvincible() {
-        rend.material.color = invincibilityColor;
-        GetComponent<PlayerDeath>().invincible = true;
-        yield return new WaitForSeconds(powerupTime);
-        GetComponent<PlayerDeath>().invincible = false;
-        rend.material.color = playerColor;
+    IEnumerator slowFall() {
+        
+        for(float t = powerupTime; t > 0; t-=Time.deltaTime) {
+            if (GetComponent<Rigidbody2D>().velocity.y < -0.1)
+            {
+                GetComponent<Rigidbody2D>().gravityScale = 0.1f;
+            }
+            yield return null;
+        }
+        resetPlayer();
     }
 }
