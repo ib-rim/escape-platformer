20c7b8c79c65909872b5fe64a41ba7237f5c8ab4
ib-rim <ib.bashir@hotmail.com>
2021-11-01 12:39:34 +0000

    Moved collectibles code into PlayerCollisions and replaced equals operator with CompareTag

--- Assets/Scripts/PlayerScripts/PlayerCollisions.cs
+++ Assets/Scripts/PlayerScripts/PlayerCollisions.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollisions : MonoBehaviour
 {
     // Code for all of the Player's non-fatal collisions 
-    
+    private static int collectiblesCounter = 0;
+    public Text collectiblesText;
+
     public int pitfallLag = 3; 
+
+    private void Start()
+    {
+        collectiblesText.text = "Collectibles: " + collectiblesCounter.ToString();
+    }
 
     IEnumerator PitfallLag() {
         yield return new WaitForSecondsRealtime(pitfallLag);
@@ -14,7 +22,7 @@ public class PlayerCollisions : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Pitfall")
+        if (collision.gameObject.CompareTag("Pitfall"))
         {
             StartCoroutine("PitfallLag");
             collision.gameObject.SetActive(false);
@@ -23,11 +31,18 @@ public class PlayerCollisions : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Checkpoint") 
+        if(other.gameObject.CompareTag("Checkpoint")) 
         {   
-            LevelManager.instance.setRespawnPoint(other.gameObject.transform);
+            LevelManager.instance.setRespawnPoint(other.gameObject.transform.position);
 
             other.gameObject.transform.Find("CheckpointMiddle").GetComponent<SpriteRenderer>().material.color = Color.cyan;
+        }
+
+        if (other.gameObject.CompareTag("Collectible"))
+        {
+            other.gameObject.SetActive(false);
+            collectiblesCounter += 1;
+            collectiblesText.text = "Collectibles: " + collectiblesCounter.ToString();
         }
     }
 }
