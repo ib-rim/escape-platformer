fcf516e2d84e42e8a4a9ff414ea65b5a309bfe5e
ib-rim <ib.bashir@hotmail.com>
2021-11-01 12:42:59 +0000

    Changed respawn to use scene load and vectors

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
-   public static LevelManager instance;
+    public static LevelManager instance;
 
-   public Transform respawnPoint;
-   public GameObject playerPrefab;
+    public static Vector2 respawnPoint = new Vector2(0, 3);
+    public GameObject playerPrefab;
 
-   private void Awake() {
-       instance = this;
+    private void Awake() {
+        instance = this;
+        playerPrefab.GetComponent<Rigidbody2D>().position = respawnPoint;
    }
 
-   public void Respawn() {
-       playerPrefab.GetComponent<Rigidbody2D>().position = respawnPoint.position;
-   }
+    public void Respawn() {
+        SceneManager.LoadScene("Main");
+    }
 
-   public void setRespawnPoint(Transform newRespawnPoint) {
-       respawnPoint = newRespawnPoint;
-   }
+    public void setRespawnPoint(Vector2 newRespawnPoint) {
+        respawnPoint = newRespawnPoint;
+    }
 }
