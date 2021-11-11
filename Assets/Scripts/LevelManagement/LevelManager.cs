66d64ae6756ad35bd3eef6d9b184f68aaa02523f
ib-rim <ib.bashir@hotmail.com>
2021-11-11 15:32:28 +0000

    Changed respawn to accommodate for multiple levels

--- Assets/Scripts/LevelManagement/LevelManager.cs
+++ Assets/Scripts/LevelManagement/LevelManager.cs
@@ -6,19 +6,39 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour {
     public static LevelManager instance;
 
-    public static Vector2 respawnPoint = new Vector2(0, 3);
-    public GameObject playerPrefab;
+    public static Vector2 respawnPoint;
+    private GameObject playerPrefab;
+    
+    public static bool levelStart = true; 
 
     private void Awake() {
-        instance = this;
+
+        playerPrefab = GameObject.Find("Player (0)");
+        DontDestroyOnLoad(gameObject);
+
+        if(instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+
+        Debug.Log(levelStart);
+        if(levelStart) {
+            respawnPoint = playerPrefab.transform.position;
+            Debug.Log(respawnPoint);
+        }
         playerPrefab.GetComponent<Rigidbody2D>().position = respawnPoint;
+        
    }
 
     public void Respawn() {
-        SceneManager.LoadScene("Main");
+        Scene scene = SceneManager.GetActiveScene(); 
+        SceneManager.LoadScene(scene.name);
     }
 
     public void setRespawnPoint(Vector2 newRespawnPoint) {
         respawnPoint = newRespawnPoint;
+        levelStart = false;
     }
 }
