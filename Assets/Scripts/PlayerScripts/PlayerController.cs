6f84d4ca194030dcc0ec551e9d9c3101eae07060
Atikah <atikah.hussain@hotmail.co.uk>
2021-11-26 10:42:14 +0000

    Add pull key controls to PlayerController.cs

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     public GameObject playerObject;
     public static Vector2 moveValue;
     public static bool isGrounded;
+    public static bool isPressingPullKey;
     public float moveSpeed;
     public float jumpSpeed;
 
@@ -27,6 +28,10 @@ public class PlayerController : MonoBehaviour
 
     public void move(InputAction.CallbackContext context) {
         moveValue = context.ReadValue<Vector2>();
+    }
+
+    public void pull(InputAction.CallbackContext context) {
+        isPressingPullKey = context.performed ? true : false;
     }
 
     public void jump(InputAction.CallbackContext context) {
