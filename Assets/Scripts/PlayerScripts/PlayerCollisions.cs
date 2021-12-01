11c2acce43409ee61da98778bc3d6d32ef0101da
ib-rim <ib.bashir@hotmail.com>
2021-12-01 22:06:57 +0000

    Removed color change of TargetPoint

--- Assets/Scripts/PlayerScripts/PlayerCollisions.cs
+++ Assets/Scripts/PlayerScripts/PlayerCollisions.cs
@@ -64,7 +64,6 @@ public class PlayerCollisions : MonoBehaviour
         {
             winText.text = "LEVEL COMPLETE";
             LevelManager.instance.setRespawnPoint(other.gameObject.transform.position);
-            other.gameObject.transform.Find("TargetpointMiddle").GetComponent<SpriteRenderer>().material.color = Color.cyan;
         }
     }
 
