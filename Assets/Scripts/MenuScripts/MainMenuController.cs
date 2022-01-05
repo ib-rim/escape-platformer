3dd701ff3cc482ab627dd68e506a5053ff474ec2
ib-rim <ib.bashir@hotmail.com>
2022-01-05 20:52:51 +0000

    Added controls menu to MainMenu scene

--- Assets/Scripts/MenuScripts/MainMenuController.cs
+++ Assets/Scripts/MenuScripts/MainMenuController.cs
@@ -7,22 +7,24 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject optionsMenu;
+    public GameObject controlsMenu;    
     public GameObject levelMenu;    
 
     private void Awake() {
+        //Skip to level select screen when returning from level
         if (PlayerPrefs.GetString("SkipToLevels") == "true") {
             play();
             PlayerPrefs.SetString("SkipToLevels", "false");
         }
     }
-
+    
     public void play()
     {
         //To level select screen
         mainMenu.SetActive(false);
         levelMenu.SetActive(true);
 
-        //Select first button on level select screen for keyboard navigation
+        //Select first Toggle on level select screen for keyboard navigation
         levelMenu.GetComponentInChildren<Toggle>().Select();
     }
 
@@ -32,8 +34,18 @@ public class MainMenuController : MonoBehaviour
         mainMenu.SetActive(false);
         optionsMenu.SetActive(true);
 
-        //Select first button on options screen for keyboard navigation
+        //Select first Slider on options screen for keyboard navigation
         optionsMenu.GetComponentInChildren<Slider>().Select();
+    }
+
+    public void controls()
+    {
+        //To options screen
+        mainMenu.SetActive(false);
+        controlsMenu.SetActive(true);
+
+        //Select first button on options screen for keyboard navigation
+        controlsMenu.GetComponentInChildren<Button>().Select();
     }
 
     //Quit game
